0f6ebe9d5d20961de5fc183ccec5e6ee6bc8f9c4
ArianaLapaix <arianablapaix@gmail.com>
2026-03-11 11:46:37 -0400

    mod 6

--- mod06_deanonymize.py
+++ mod06_deanonymize.py
@@ -18,8 +18,18 @@ def link_records(anon_df, aux_df):
       anon_id, matched_name
     containing ONLY uniquely matched records.
     """
-    raise NotImplementedError
-
+    merged = pd.merge(
+        anon_df,
+        aux_df,
+        on=['age', 'gender', 'zipcode'],
+        how='inner',
+        suffixes=('_anon', '_aux')
+    )
+    match_counts = merged.groupby('anon_id').size()
+    unique_ids = match_counts[match_counts == 1].index
+    result = merged[merged['anon_id'].isin(unique_ids)][['anon_id', 'name']]
+    result = result.rename(columns={'name': 'matched_name'})
+    return result
 
 def deanonymization_rate(matches_df, anon_df):
     """
@@ -27,3 +37,6 @@ def deanonymization_rate(matches_df, anon_df):
     that were uniquely re-identified.
     """
     raise NotImplementedError
+
+
+#  write functions in "deanonymize" script to (1) link the records in the two datasets and (2) calculate the rate at which you are successful doing so. 
